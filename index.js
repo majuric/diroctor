ce3474407807138992997e9e8d24181289a00f66
Mario Juric <mario.jur@gmail.com>
2018-10-22 16:23:12 +0200

    Restore the repository structure based on .diroctor.lock

--- index.js
+++ index.js
@@ -1,18 +1,28 @@
-const dirTree = require("./lib/directoryTree");
-const treeFile = ".diroctor.lock";
 const homedir = require("os").homedir();
 const path = require("path");
 const fs = require("fs");
+const program = require("commander");
+const treeFile = ".diroctor.lock";
+const dirTree = require("./lib/directoryTree");
+const restore = require("./lib/restore");
+const config = require("./lib/config");
 
-// Get the tree as JSON
-const tree = dirTree("code", { exclude: /node_modules/ })
-  .then(tree => {
-    const util = require("util");
+program
+  .version("0.1.0")
+  .option("-i, --install", "Re-install repository directory tree")
+  .parse(process.argv);
 
-    //   console.log(util.inspect(tree, false, null, true /* enable colors */));
-
-    fs.writeFileSync(path.resolve(homedir, treeFile), JSON.stringify(tree, null, 2), "utf-8");
-  })
-  .catch(error => {
-    console.error(error);
-  });
+if (program.install) {
+  console.log("Re installing the repository directory tree");
+  restore();
+} else {
+  console.log("Storing the new repository directory tree");
+  // Get the tree as JSON
+  dirTree(config.SOURCE_PATH, { exclude: /node_modules/ })
+    .then(tree => {
+      fs.writeFileSync(path.resolve(homedir, treeFile), JSON.stringify(tree, null, 2), "utf-8");
+    })
+    .catch(error => {
+      console.error(error);
+    });
+}
